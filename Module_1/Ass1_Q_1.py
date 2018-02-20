40f1c2863e1203d475445dd5d19b451c5cb1368a
@Sam <soumen.ugent@gmail.com>
2018-02-20 16:31:33 +0530

    Assignment-1

---
+++ Module_1/Ass1_Q_1.py
@@ -0,0 +1,5 @@
+s='Discover,Learning,with,Edureka'
+print"Number of lowercase a:",s.count('a',0,len(s))
+print"Number of lowercase o:",s.count('a',0,len(s))
+print"Number of uppercase L:",s.count('L',0,len(s))
+print"Number od uppercase N:",s.count('N',0,len(s))
